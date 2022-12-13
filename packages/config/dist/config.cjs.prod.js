6c2100887f388a8b29da2d6e58646458c243f1da
yejin <8058883@qq.com>
2022-12-13 09:32:10 +0800

    release: v0.0.2

---
+++ packages/config/dist/config.cjs.prod.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const createField = function () {
+    return {};
+};
+
+exports.createField = createField;
---
+++ packages/config/dist/config.esm-bundler.js
@@ -0,0 +1,5 @@
+const createField = function () {
+    return {};
+};
+
+export { createField };
